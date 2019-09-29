a2d418f3719fefe2e7ae467da89aa136ed268cb5
momotomo <tomoya@TomoyanoMacBook-Pro.local>
2019-09-29 21:55:30 +0900

    base code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Form from './Form';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  );
+}
+
+export default App;
